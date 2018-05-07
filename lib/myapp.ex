70bd7859cb177f2c8d6eed0e06803c8c805d7648
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-07 13:52:49 +0900

    Test elixir

---
+++ lib/myapp.ex
@@ -0,0 +1,20 @@
+defmodule Myapp do
+  @moduledoc """
+  Documentation for Myapp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Myapp.hello
+      :world
+
+  """
+  def hello do
+    # :world
+    # "We need to make this test failed"
+    :goodbye
+  end
+end
