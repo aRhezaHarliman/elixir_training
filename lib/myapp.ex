804036f60d79c70f21d5254d9f67f13511e5eab4
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-07 14:59:45 +0900

    Add hipchat cli

--- lib/myapp.ex
+++ lib/myapp.ex
@@ -12,9 +12,34 @@ defmodule Myapp do
       :world
 
   """
-  def hello do
-    :world
-    # "We need to make this test failed"
-    # :goodbye
+  # def hello do
+  #   :world
+  #   # "We need to make this test failed" -> uncomment this one then the test will be failed
+  #   # :goodbye -> uncomment this one then the test will be failed
+  # end
+
+  use Application
+
+  def start(_type, _args) do
+    Myapp.Supervisor.start_link()
   end
+
+  alias Hipchat.ApiClient
+  alias Hipchat.Httpc.Response
+  alias Hipchat.V2.Rooms
+
+  def main([room_id, msg]) do
+    client = ApiClient.new(load_token())
+    {:ok, %Response{status: 201}} = Rooms.send_message(client, room_id, %{message: msg})
+  end
+
+  defp load_token() do
+    Path.expand("~/.config/hipchat_cli/token")
+    |> File.read!()
+    |> String.trim()
+  end
+
+  # def main(_) do
+  #   IO.puts("Hello Elixir!")
+  # end
 end
--- mix.exs
+++ mix.exs
@@ -7,13 +7,15 @@ defmodule Myapp.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      escript: [main_module: Myapp]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Myapp, []},
       extra_applications: [:logger]
     ]
   end
@@ -21,6 +23,9 @@ defmodule Myapp.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:mix_test_watch, "0.6.0", [only: :dev, runtime: false]},
+      {:hipchat_elixir, "0.2.3"},
+      {:poison, "2.2.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
